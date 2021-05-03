7858e84d172d70aa1a77974fb3843cd32b9d35fe
Israel Zexer <israzex778@gmail.com>
2021-05-03 21:33:27 +0300

    Developer controller was created

---
+++ vapor/Controllers/DevelopersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using vapor.Data;
+using vapor.Models;
+
+namespace vapor.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        private readonly vaporContext _context;
+
+        public DevelopersController(vaporContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Developers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Developer.ToListAsync());
+        }
+
+        // GET: Developers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var developer = await _context.Developer
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (developer == null)
+            {
+                return NotFound();
+            }
+
+            return View(developer);
+        }
+
+        // GET: Developers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Developers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,name")] Developer developer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(developer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(developer);
+        }
+
+        // GET: Developers/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var developer = await _context.Developer.FindAsync(id);
+            if (developer == null)
+            {
+                return NotFound();
+            }
+            return View(developer);
+        }
+
+        // POST: Developers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("id,name")] Developer developer)
+        {
+            if (id != developer.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(developer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DeveloperExists(developer.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(developer);
+        }
+
+        // GET: Developers/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var developer = await _context.Developer
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (developer == null)
+            {
+                return NotFound();
+            }
+
+            return View(developer);
+        }
+
+        // POST: Developers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var developer = await _context.Developer.FindAsync(id);
+            _context.Developer.Remove(developer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DeveloperExists(string id)
+        {
+            return _context.Developer.Any(e => e.id == id);
+        }
+    }
+}
