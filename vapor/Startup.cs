38192fe4e98b2767ffa9bd076701d87fd51cb598
yohai knaani <yohai109@gmail.com>
2021-05-22 11:48:20 +0300

    change home screen to game list

--- vapor/Startup.cs
+++ vapor/Startup.cs
@@ -55,7 +55,7 @@ namespace vapor
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=games}/{action=Index}/{id?}");
             });
         }
     }
