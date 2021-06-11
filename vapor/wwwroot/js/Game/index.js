3a3c1dae82f59df72d98da4750f7d5927319d5f4
yohai knaani <yohai109@gmail.com>
2021-06-11 21:21:59 +0300

    genres and developers fields are now check boxes

--- vapor/wwwroot/js/Game/index.js
+++ vapor/wwwroot/js/Game/index.js
@@ -1,19 +1,34 @@
 ﻿$(function () {
 
-    $("#search-btn").click(function (e) {
+    /*$("#search-btn").submit(function (e) {*/
+    $("#search-container").submit(function (e) {
         console.log("searching");
         e.preventDefault();
 
         var textQuery = $("#search-box").val();
-        var genres = $("#genres-select").val() || [];
-        var developers = $("#developer-select").val() || [];
+        var genres = [];
+        var developers = [];
 
-        console.log(genres);
+        $.each($("#search-container").serializeArray(), function (i, currElement) {
+            switch (currElement.name) {
+                case "genres-select":
+                    genres.push(currElement.value);
+                    break;
+                case "developer-select":
+                    developers.push(currElement.value)
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        var data = { query: textQuery, developers: developers, genres: genres };
+        console.log(data);
 
         $.ajax({
             method: "POST",
             url: '/Games/Search',
-            data: { query: textQuery, developers: developers, genres: genres }
+            data: data
         }).done(function (data) {
             $('#game-list').html('');
             var gameTemplate = $('#game_template').html();
@@ -29,7 +44,7 @@
                 })
 
                 gameTemp = gameTemp.replaceAll('{genre}', genreFinal)
-                console.log(gameTemp)
+
                 $.each(val, function (key, value) {
                     gameTemp = gameTemp.replaceAll('{' + key + '}', value)
                 })
