522798d0790cace7b180d14f3dfbe896f512bf75
aradgonen98 <gonena.arad@gmail.com>
2021-06-18 19:19:04 +0300

    added form validation and fix the user registration

--- vapor/Controllers/UsersController.cs
+++ vapor/Controllers/UsersController.cs
@@ -185,7 +185,19 @@ namespace vapor.Controllers
 
 
         }
-
+        [HttpPost]
+        public String checkIfUsernameExist(String username)
+        {
+            var user_check = _context.User.Where(u => u.Username == username).FirstOrDefault();
+            if (user_check != null)
+            {
+                return "Taken";
+            }
+            else
+            {
+                return "OK";
+            }
+        }
         [Authorize(Roles = "Admin")]
         // GET: Users
         public async Task<IActionResult> Index()
