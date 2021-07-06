6c39d52e4952b15261db61413b6462da790964e8
Israel Zexer <israzex777@gmail.com>
2021-07-07 02:01:36 +0300

    feat: Image animated item-bar and Screen where implemented

---
+++ vapor/wwwroot/js/Games/ImageDisplay.js
@@ -0,0 +1,35 @@
+﻿var curSelectedItem;
+
+$(document).ready(function () {
+    $(".item-bar__item").click(selectItem)
+
+
+})
+
+function selectItem() {
+    let itemBar = $(this).parent()
+    let SLECTED_ITEM_CLASS = "item-bar__item--selected"
+    let itemId = ""
+
+    let newSelectedItem = $(this).attr("data-image-url")
+    console.log(newSelectedItem)
+    console.log(curSelectedItem)
+    // If update should occurre
+    if (curSelectedItem === newSelectedItem) {
+        return
+    }
+
+    // Updates the selected item stlyes
+    let allItems = itemBar.children()
+    let newItem = itemBar.children(`.item-bar__item[data-image-url='${newSelectedItem}']`)
+
+    allItems.removeClass(SLECTED_ITEM_CLASS)
+    newItem.addClass(SLECTED_ITEM_CLASS)
+
+    curSelectedItem = newSelectedItem
+
+    console.log(curSelectedItem)
+
+    // Updates the selected item display
+    $(".image-screen img").attr("src", newSelectedItem)
+}
