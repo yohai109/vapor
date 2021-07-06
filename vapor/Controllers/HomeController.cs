9d055a6aad3b11e9e985f7ae073154e8613f360a
aradgonen98 <gonena.arad@gmail.com>
2021-07-06 19:36:28 +0300

    added 404 handling

--- vapor/Controllers/HomeController.cs
+++ vapor/Controllers/HomeController.cs
@@ -47,5 +47,9 @@ namespace vapor.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult NotFound()
+        {
+            return View("NotFound", "Home");
+        }
     }
 }
