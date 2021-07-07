8aa151905ee23262d04b573239d5d34d32d1bd72
Israel Zexer <israzex777@gmail.com>
2021-07-07 03:38:10 +0300

    feat: Game image carousel with item-bar rotate was implemented

--- vapor/wwwroot/js/Games/ImageDisplay.js
+++ vapor/wwwroot/js/Games/ImageDisplay.js
@@ -1,19 +1,44 @@
 ﻿var curSelectedItem;
+var barRowPossition = 0;
 
-$(document).ready(function () {
-    $(".item-bar__item").click(selectItem)
+function calculateRotateSize() {
+    let vm = 15.4
+
+    // Convers vm to pixcels
+
+    let vmSize = document.documentElement.clientWidth / 100
+    return vm * vmSize
+}
+
+function souldRotateRight(direction) {
+    let displayWidth = $(".image-display").width()
+    let barWidth = $(".item-bar").width()
+    return Math.abs(barRowPossition) + displayWidth < barWidth
+}
+
+function souldRotateLeft(direction) {
+    return barRowPossition < 0
+}
 
 
-})
+/*
+    Rolls the item bar
+*/
+function rotateItemBar(amount) {
+    barRowPossition += amount
+    $(".carousel .item-bar").css("transform", `translateX(${barRowPossition}px)`)
+}
 
+/*
+    Selects another image
+*/
 function selectItem() {
     let itemBar = $(this).parent()
     let SLECTED_ITEM_CLASS = "item-bar__item--selected"
     let itemId = ""
 
     let newSelectedItem = $(this).attr("data-image-url")
-    console.log(newSelectedItem)
-    console.log(curSelectedItem)
+
     // If update should occurre
     if (curSelectedItem === newSelectedItem) {
         return
@@ -28,8 +53,24 @@ function selectItem() {
 
     curSelectedItem = newSelectedItem
 
-    console.log(curSelectedItem)
-
     // Updates the selected item display
     $(".image-screen img").attr("src", newSelectedItem)
 }
+
+
+$(document).ready(function () {
+    $(".item-bar__item").click(selectItem)
+    $(".carousel__arrow--right").click(() => {
+        if (souldRotateRight()) {
+            let rotateSize = calculateRotateSize()
+            rotateItemBar(-rotateSize)
+        }
+    })
+    $(".carousel__arrow--left").click(() => {
+        if (souldRotateLeft()) {
+            let rotateSize = calculateRotateSize()
+            rotateItemBar(rotateSize)
+        }
+    })
+    rotateItemBar(0)
+})
