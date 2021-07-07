1857b93b729a79fa8afd95381080087629e7d709
aradgonen98 <gonena.arad@gmail.com>
2021-07-07 23:51:33 +0300

    fix twitter

--- vapor/services/twitter.cs
+++ vapor/services/twitter.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using vapor.Models;
 
@@ -10,13 +13,18 @@ namespace vapor.services
     public class twitter
     {
         private HttpClient client;
+        private string standard_url;
         private string url;
-
+        private RestClient restClient;
 
         public twitter()
         {
             client = new HttpClient();
-            url = "https://v1.nocodeapi.com/vapor/twitter/qKGXDIYbBwqJkPxZ";
+            standard_url = "https://v1.nocodeapi.com/vapor/twitter/qKGXDIYbBwqJkPxZ";
+
+            url = "https://app.ayrshare.com/api/post";
+            restClient = new RestClient("https://app.ayrshare.com/api/post");
+
         }
 
         public void postTweet(Game newGame)
@@ -32,13 +40,25 @@ namespace vapor.services
 
         public async void postTweet(string status)
         {
+            //Alternative Way
+            restClient.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer C0BJX5N-8WC4BHC-HM83R64-141206Y");
+            var body = "{\"post\": \"" + status + "\", \"platforms\": [\"twitter\"]}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = restClient.Execute(request);
+            Console.WriteLine(response.Content);
+
+            //Standard Way
             var values = new Dictionary<string, string> { };
             var content = new FormUrlEncodedContent(values);
 
-            var response = await client.PostAsync(url + "?status=" + status, content);
+            var standard_response = await client.PostAsync(standard_url + "?status=" + status, content);
 
-            var responseString = await response.Content.ReadAsStringAsync();
+            var responseString = await standard_response.Content.ReadAsStringAsync();
             Console.WriteLine(responseString);
+
         }
-    }
+}
 }
