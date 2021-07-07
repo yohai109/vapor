8c19319689abd03820abd5786f5e1a0f1bd207ef
Israel Zexer <israzex777@gmail.com>
2021-07-07 22:26:10 +0300

    fix: Sidbar search in home page, developer presents the selected number

--- vapor/wwwroot/js/Developer/Index.js
+++ vapor/wwwroot/js/Developer/Index.js
@@ -1,38 +1,4 @@
-﻿/**
-
-function convertImageFrom64(avatar, type) {
-    var image = new Image();
-    image.src = 'data:' + type + ';base64,' + avatar;
-    return image.src;
-}
-
-$("body", function insertIntoPage() {
-    $.ajax({
-        url: '/Developers/All'
-    }).done(function (data) {
-        $('tbody').html('');
-        var template = $('#developer_template').html();
-
-        $.each(data, function (i, val) {
-            var temp = template;
-            temp = temp.replaceAll("{image}", "<img src='" + convertImageFrom64(val.avatar, val.fileContentType) + "'></img>");
-            $.each(val, function (key, value) {
-                temp = temp.replaceAll('{' + key + '}', value)
-            })
-
-            console.log(temp)
-
-            $('tbody').append(
-                temp 
-            )
-        })
-    })
-})
-*/
-
-
-$(function () {
-
+﻿$(function () {
     $("#search-container").submit(function (e) {
         
         e.preventDefault();
@@ -42,7 +8,6 @@ $(function () {
         var numOfGames = $("#num-of-games").val();
 
         var data = { devName: devName, gameName: gameName, numOfGames: numOfGames };
-        console.log(data);
 
         $.ajax({
             method: "POST",
@@ -52,21 +17,8 @@ $(function () {
             $('#developer-list').html('');
             var developerTemplate = $('#developer-template').html();
 
-            console.log("received:")
-            console.log(data)
-            /*var genreTemplate = $('#genre-template').html();*/
-
             $.each(data, function (i, val) {
                 var devTemp = developerTemplate;
-
-                /*var genreFinal = ""
-
-                $.each(val.genres, function (i, currGener) {
-                    genreFinal += genreTemplate.replaceAll("{name}", currGener.name);
-                })
-
-                gameTemp = gameTemp.replaceAll('{genre}', genreFinal)*/
-
                 $.each(val, function (key, value) {
                     devTemp = devTemp.replaceAll('{' + key + '}', value)
                 })
